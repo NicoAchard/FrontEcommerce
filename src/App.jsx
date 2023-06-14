31c7437270b49f6e55415db7292c2678c593d846
RamiroVidal26 <110636851+RamiroVidal26@users.noreply.github.com>
2023-06-14 16:53:03 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <NavBar /> {/* Agrega el componente NavBar aquí */}
       <Routes>
         <Route path="/login" />
         <Route path="/signup" />
         <Route path="/admin" />
+        <Route path="/NavBar" element={NavBar} />
         <Route path="/home" />
         <Route path="/products" />
         <Route path="/about-this-proyect" />
-
         <Route path="*" />
       </Routes>
     </>
