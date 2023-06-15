568460ba9c22fbddcac45bd2760ed7b979e60022
dievaliente <dievaliente@gmail.com>
2023-06-15 09:56:22 -0300

    team card

---
+++ src/pages/TeamCard.jsx
@@ -0,0 +1,27 @@
+import Card from "react-bootstrap/Card";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+
+function TeamCard({ img, name, academicInfo, description }) {
+  return (
+    <div className="col-lg-3 border rounded card ps-0 m-4">
+      <Card style={{ width: "24rem" }}>
+        <Card.Img
+          variant="top"
+          src={img}
+          className="rounded"
+          style={{ height: "300px", objectFit: "cover" }}
+        />
+        <Card.Body>
+          <Card.Title className="pb-1">{name}</Card.Title>
+          <Card.Subtitle className="pb-3">{academicInfo}</Card.Subtitle>
+          <Card.Text>{description}</Card.Text>
+          <BsGithub className="m-2 fs-4" />
+          <BsLinkedin className="m-2 fs-4" />
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default TeamCard;
