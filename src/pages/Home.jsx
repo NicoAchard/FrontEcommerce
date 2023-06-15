262555fd0ca501bc89522f40810e376759fb0b53
NicoAchard <achardn@gmail.com>
2023-06-15 17:20:59 -0300

    CallToAction to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Categories from "../components/Categories";
 import ProductsList from "../components/ProductsList";
+import CallToAction1 from "../components/CallToAction1";
+import CallToAction2 from "../components/CallToAction2";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <CallToAction1 />
       <Categories />
+      <CallToAction2 />
       <ProductsList />
       <Footer />
     </div>
