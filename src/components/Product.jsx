90302f254036501d931679d56dab3c74f75bac0d
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-06-16 22:50:32 -0300

    Carrusel without arrows when does't have more than 1 img

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Carousel from "react-bootstrap/Carousel";
-import "./Product.css"; // Import your custom CSS file
+import "./Product.css";
 
 function Product({ product }) {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -12,17 +12,26 @@ function Product({ product }) {
 
   return (
     <Card style={{ width: "18rem", border: "none" }}>
-      <Carousel activeIndex={activeIndex} onSelect={handleSlide} interval={null}>
-        {product.photos.map((photo) => (
-          <Carousel.Item key={photo.id}>
-            <img
-              className="d-block w-100"
-              src={`${import.meta.env.VITE_API_IMG}/${photo.url}`}
-              alt={product.name}
-            />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      {product.photos.length > 1 ? (
+        <Carousel activeIndex={activeIndex} onSelect={handleSlide} interval={null}>
+          {product.photos.map((photo) => (
+            <Carousel.Item key={photo.url}>
+              <img
+                className="d-block w-100"
+                src={`${import.meta.env.VITE_API_IMG}/${photo.url}`}
+                alt={product.name}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      ) : (
+        <img
+          className="d-block w-100"
+          src={`${import.meta.env.VITE_API_IMG}/${product.photos[0].url}`}
+          alt={product.name}
+        />
+      )}
+
       <Card.Body style={{ height: "250px" }}>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{product.description.slice(0, 150)} . . .</Card.Text>
