0834b4a722e80d4ebfd7b5cd6f6819d18e1ab957
dievaliente <dievaliente@gmail.com>
2023-06-22 10:30:41 -0300

    add chart to Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import NavAdmin from "../components/NavAdmin";
 import DashboardCard from "../components/DashboardCard";
 import DashboardPopular from "../components/DashboardPopular";
 import DashboardUsers from "../components/DashboardUsers";
+import SalesChart from "../components/SalesChart";
 import "./Dashboard.css";
 
 export default () => (
@@ -14,10 +15,13 @@ export default () => (
         <div className="dashboard-space ">
           <div classname="d-flex">
             <div className="d-flex px-1">
-              <div className="d-flex w-100 h-50">
-                <DashboardCard url="orders" title="Orders" icon="src\img\order.png" />
-                <DashboardCard url="users" title="Users" icon="src\img\user.png" />
-                <DashboardCard url="products" title="Products" icon="src\img\skateboard.png" />
+              <div className="d-flex flex-column">
+                <div className="d-flex w-100 h-50">
+                  <DashboardCard url="orders" title="Orders" icon="src\img\order.png" />
+                  <DashboardCard url="users" title="Users" icon="src\img\user.png" />
+                  <DashboardCard url="products" title="Products" icon="src\img\skateboard.png" />
+                </div>
+                <SalesChart />
               </div>
               <div className="popular border rounded mt-2 me-1">
                 <DashboardPopular />
