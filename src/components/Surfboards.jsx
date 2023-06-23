8190a56dcd804f49f95cce9bfca9f054f83a492a
dievaliente <dievaliente@gmail.com>
2023-06-23 15:05:03 -0300

    restore changes

--- src/components/Surfboards.jsx
+++ src/components/Surfboards.jsx
@@ -15,7 +15,7 @@ function Surfboards() {
           />
         </div>
         <div
-          className="col=md-6 pe-0"
+          className="col-md-6 pe-0"
           style={{
             height: "550px",
           }}
