6a87f252f5711ab8465b69c6225fef3bc1b1012c
NicoAchard <achardn@gmail.com>
2023-06-28 15:59:47 -0300

    callToAction

--- src/components/CallToAction1.jsx
+++ src/components/CallToAction1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./CallToAction.css";
+
 import { Link, useNavigate } from "react-router-dom";
 
 function CallToAction1() {
--- src/components/CallToAction2.jsx
+++ src/components/CallToAction2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./CallToAction.css";
+import "./CallToAction2.css";
 import { useNavigate } from "react-router-dom";
 
 function CallToAction2() {
