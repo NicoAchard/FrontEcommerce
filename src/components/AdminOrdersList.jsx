79ed668db7b62e8e38a510a3359f361aa37772a3
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-06-30 12:49:05 -0300

    Popular dashboard

--- src/components/AdminOrdersList.jsx
+++ src/components/AdminOrdersList.jsx
@@ -17,12 +17,10 @@ function AdminOrdersList() {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response.data);
       setOrders(response.data);
     };
     getAll();
   }, []);
-
   return (
     <div className="p-4 ">
       <table className="table border rounded">
